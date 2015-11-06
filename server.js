640146a4f9f5c3b600755effee5c3b5e4ea903d7
eformat <eformat@gmail.com>
2015-11-06 19:17:55 +1300

    first commit

---
+++ server.js
@@ -0,0 +1,33 @@
+'use strict';
+/**************************************************************************************************
+ * This sample demonstrates the most simplistic usage of Swagger Server.
+ * It simply creates a new instance of Swagger Server and starts it.
+ * All functionality is provided automatically by Swagger Server's mocks.
+ * Validate and Parse the API yaml/json before starting
+ **************************************************************************************************/
+
+// Set the DEBUG environment variable to enable debug output
+process.env.DEBUG = 'swagger:*';
+
+// Create a Swagger Server app from the PetStore.yaml file
+var swaggerServer = require('swagger-server');
+var SwaggerParser = require('swagger-parser');
+
+var myAPI = 'HelloWorld.yaml';
+
+var app = swaggerServer(myAPI);
+
+// Validate it first
+SwaggerParser.validate(myAPI)
+    .then(function(api) {
+      console.log("API name: %s, Version: %s", api.info.title, api.info.version);
+
+      // Start listening on port 8000
+      app.listen(8080, function() {
+          console.log('HelloWorld is now running at http://localhost:8080');
+      });
+    })
+    .catch(function(err) {
+      console.error(err);
+    });
+
